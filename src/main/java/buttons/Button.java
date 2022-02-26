65a6e6e7e1d2ef73b907be675a8aa692529cef01
Colin Brennan <cbrennan1@hawk.iit.edu>
2022-02-26 16:59:11 -0600

    Update Button.java
    Comments from assignment instructions
    Spacing

--- src/main/java/buttons/Button.java
+++ src/main/java/buttons/Button.java
@@ -2,26 +2,25 @@
 package main.java.buttons;
 
 public class Button implements SwitchInterface {
+	
+	// When switched on the Button will print "Button switched to ON" to stdout. 
+	// When switched off the button will print "Button switched to OFF" to stdout.
     private final static String switchOnString = "Button switched to ON";
     private final static String switchOffString = "Button switched to OFF";
 
+    // The Button knows how to respond to switchOn() and switchOff()
     @Override
     public void switchOn() {
-        System.out.println(switchOnString);
-    }
-
+        System.out.println(switchOnString); }
     @Override
     public void switchOff() {
-        System.out.println(switchOffString);
-    }
-
+        System.out.println(switchOffString); }
+    
+    //  Get SwitchOn/SwitchOff String
     @Override
     public String getSwitchOnString() {
-        return switchOnString;
-    }
-
+        return switchOnString; }
     @Override
     public String getSwitchOffString() {
-        return switchOffString;
-    }
+        return switchOffString; }
 }
