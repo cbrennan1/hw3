4cabc350554d095cb42a6c5d65b4e000271bd812
Colin Brennan <cbrennan1@hawk.iit.edu>
2022-02-26 19:28:28 -0600

    Create TableLamp_Testing.java

---
+++ src/test/java/TableLamp_Testing.java
@@ -0,0 +1,14 @@
+package test.java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TableLamp_Testing {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
