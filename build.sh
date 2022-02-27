4481e12bb14955b4d58eeaa3fd701f7c79540351
Colin Brennan <cbrennan1@hawk.iit.edu>
2022-02-26 19:54:51 -0600

    build.sh
    build.sh

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get -y update
+sudo apt-get -y install openjdk-17-jdk
+
+
+sudo snap install gradle --classic
+sudo chmod +x gradlew
+./gradlew clean build
+./gradlew test
+./gradlew jacocoTestReport
